24fde51d70e69c0379d156f43f4eddbbf0f30380
VitorSantos29 <vg19926@gmail.com>
2026-08-21 00:41:15 -0300

    novo projeto

---
+++ index.js
@@ -0,0 +1,31 @@
+let herois = [
+    { nome: "Arthur", xp: 500 },
+    { nome: "Carlos", xp: 2500 },
+    { nome: "João", xp: 6500 },
+    { nome: "Lucas", xp: 12000 }
+];
+
+for (let i = 0; i < herois.length; i++) {
+    let nivel = "";
+    let xp = herois[i].xp;
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log(`O Herói de nome ${herois[i].nome} está no nível de ${nivel}`);
+}
